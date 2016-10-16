4f185d293eb17a341175694f72ce69f3fc6e5382
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-16 20:03:46 +0200

    Basic ajax query completed

--- assets/scripts/ajax_omdb.js
+++ assets/scripts/ajax_omdb.js
@@ -2,6 +2,34 @@ $(document).ready(function(){
 	console.log("ready");
 	$('#submit_button').click(function(event){
 		event.preventDefault();
-		alert($('#title').val());
+		//alert($('#title').val());
+		//if ($('#title').val() != ""){
+			omdbQuery(); 
+		//}
 	});
 });
+
+var omdbQuery = function(){
+	console.log($('#title').val());
+	//var queryString = "http://www.omdbapi.com/?t=amelia&y=&plot=short&r=json";
+	$.ajax({
+		url: "http://www.omdbapi.com/?",
+		data: {
+			s: $('#title').val(),
+			page: ""
+		},
+		type: "GET",
+		dataType: "json",
+
+	}).done(function( json ) {
+     $( "<h1>" ).text( json.Title ).appendTo( "#results" );
+     //$( "<div class=\"content\">").html( json.html ).appendTo( "body" );
+     $(`<h3>${json.Director}</h3>`).appendTo("#results");
+        alert( "The request is complete!" );
+	}).fail(function( xhr, status, errorThrown ) {
+	   alert( "Sorry, there was a problem!" );
+	   console.log( "Error: " + errorThrown );
+	   console.log( "Status: " + status );
+	   console.dir( xhr );
+	});
+};
